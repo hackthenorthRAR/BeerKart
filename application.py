bc3bbe648a9ef9017453d394a679e6ec55071348
Rohan <rohancalgary14@gmail.com>
2017-09-16 10:22:42 -0400

    updated application.py and helpers.py

--- application.py
+++ application.py
@@ -1,5 +1,23 @@
 from flask import Flask, render_template
+from flask_session import Session
+
 app = Flask(__name__)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 # import the driver
 import psycopg2
 
@@ -33,3 +51,12 @@ def index():
     conn.close()
 
     return render_template('index.html')
+
+@app.route("/login")
+def login():
+
+# insert google Authorization
+
+@app.route("/pickup", methods=["GET", "POST"])
+@login_required
+def pickup():
